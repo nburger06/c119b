532433d49201dab37082af95323ffaf0913a57e7
nburger06 <nburger0691@gmail.com>
2026-02-04 22:41:50 -0800

    more comments

--- example.r
+++ example.r
@@ -25,25 +25,30 @@ P <- matrix( #row = current state, col = next state
 w <- matrix(1, 3, 3) #create a 3x3 neighborhood weight matrix
 w[2, 2] <- 0
 
-neighbor_frac <- function(r, state) {
-  focal(r == state, w, sum, na.rm = TRUE) / 8 #terra::focal(), compute moving window values for each cell. window must have odd dims. 
+neighbor_frac <- function(r, state) { #computes fraction of neighboring cells in given state
+  focal(r == state, w, sum, na.rm = TRUE)  / 8 #terra::focal(), compute moving window values for each cell. window must have odd dims. 
 } # w = window/weight matrix, sum = function. 
 
-modify_P <- function(P, mature_frac) { #injects spatial component
+modify_P <- function(P, mature_frac, undist_frac) { #injects spatial component . why choice of 0.3? arbitrary?
   P2 <- P
   P2[3, 3] <- P2[3, 3] + 0.3 * mature_frac #3,3 is prob of going from F to F. increases depending on mature_frac
-  P2[3, ] <- P2[3, ] / sum(P2[3, ]) # rescale all transitions so they sum to 1.
+  P2[3, 4] <- P2[3, 4] + 0.3 * undist_frac #3,4 is prob of going from F to R. increases depending on undist_frac
+  P2[2, 1] <- P2[2, 1] + 0.2 * undist_frac #2,1 is prob of going from G to U. increases depending on undist_frac
+  P2[3, ] <- P2[3, ] / sum(P2[3, ]) # rescale all transitions from F so they sum to 1.
+  P2[2, ] <- P2[2, ] / sum(P2[2, ]) # rescale all transitions from G so they sum to 1.
   P2
 }
 
 step_forest <- function(r) {
-  mf <- neighbor_frac(r, 3)
-  r_new <- r
+  mf <- neighbor_frac(r, 3) #sum neighbor states that are deforested (state 3)
+  mu <- neighbor_frac(r, 1) #sum neighbor states that are undisturbed (state 1)
+  r_new <- r 
   
-  for (i in 1:ncell(r)) {
-    s <- values(r)[i]
-    Pi <- modify_P(P, values(mf)[i])[s, ]
-    values(r_new)[i] <- sample(1:4, 1, prob = Pi)
+  for (i in 1:ncell(r)) { #for each cell
+    s <- values(r)[i] #get state
+    Pi <- modify_P(P, values(mf)[i], values(mu)[i])[s, ]  #get fraction of F neighbors, modify P matrix, extract row s
+
+    values(r_new)[i] <- sample(1:4, 1, prob = Pi) # sample new state based on new P
   }
   
   r_new
@@ -63,11 +68,11 @@ forest_cols <- c(
 
 png("forest_map.png", width=800, height=800)  # open device
 terra::plot(
-    r,
-    col = forest_cols,
-    axes = FALSE,
-    main = paste("Timestep", t)
-  )  
+  r,
+  col = forest_cols,
+  axes = FALSE,
+  main = paste("Timestep", 0)
+)
 dev.off()     
 for (t in 1:10) {
   cat("Simulating timestep", t, "...\n")  # print progress
@@ -81,7 +86,6 @@ for (t in 1:10) {
   )  
   dev.off()     
 }
-
 # save as PNG
      
 
